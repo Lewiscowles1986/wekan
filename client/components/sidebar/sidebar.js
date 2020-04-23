89cd3c70f66c23e054e698725fa69955144e864d
Lewis Cowles <lewiscowles@me.com>
2020-04-23 20:35:59 +0100

    Export to Zip

--- client/components/sidebar/sidebar.js
+++ client/components/sidebar/sidebar.js
@@ -1,4 +1,5 @@
 import { Cookies } from 'meteor/ostrio:cookies';
+const JSZip = require('jszip');
 const cookies = new Cookies();
 Sidebar = null;
 
@@ -12,6 +13,146 @@ const viewTitles = {
   multiselection: 'multi-selection',
   customFields: 'custom-fields',
   archives: 'archives',
+};
+
+const saveAs = function(blob, filename) {
+  let dl = document.createElement('a');
+  dl.href = window.URL.createObjectURL(blob);
+  dl.onclick = event => document.body.removeChild(event.target);
+  dl.style.display = 'none';
+  dl.target = '_blank';
+  dl.download = filename;
+  document.body.appendChild(dl);
+  dl.click();
+};
+
+const asyncForEach = async function (array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+};
+
+const getPageHtmlString = () => {
+  return `<!doctype html>${
+    window.document.querySelector('html').outerHTML
+  }`;
+};
+
+const removeAnchors = htmlString => {
+  const replaceOpenAnchor = htmlString.replace(new RegExp('<a ', 'gim'), '<span ');
+  return replaceOpenAnchor.replace(new RegExp('<\/a', 'gim'), '</span');
+};
+
+const ensureSidebarRemoved = () => {
+  document.querySelector('.board-sidebar.sidebar').remove();
+};
+
+const addJsonExportToZip = async (zip, boardSlug) => {
+  const downloadJSONLink = document.querySelector('.download-json-link');
+  const downloadJSONURL = downloadJSONLink.href;
+  const response = await fetch(downloadJSONURL);
+  const responseBody = await response.text();
+  zip.file(`data/${boardSlug}.json`, responseBody);
+};
+
+const closeSidebar = () => {
+  document.querySelector('.board-header-btn.js-toggle-sidebar').click();
+};
+
+const cleanBoardHtml = () => {
+  Array.from(document.querySelectorAll('script')).forEach(elem =>
+    elem.remove(),
+  );
+  Array.from(
+    document.querySelectorAll('link:not([rel="stylesheet"])'),
+  ).forEach(elem => elem.remove());
+  document.querySelector('#header-quick-access').remove();
+  Array.from(
+    document.querySelectorAll('#header-main-bar .board-header-btns'),
+  ).forEach(elem => elem.remove());
+  Array.from(document.querySelectorAll('.list-composer')).forEach(elem =>
+    elem.remove(),
+  );
+  Array.from(
+    document.querySelectorAll(
+      '.list-composer,.js-card-composer, .js-add-card',
+    ),
+  ).forEach(elem => elem.remove());
+  Array.from(
+    document.querySelectorAll('.js-perfect-scrollbar > div:nth-of-type(n+2)'),
+  ).forEach(elem => elem.remove());
+  Array.from(document.querySelectorAll('.js-perfect-scrollbar')).forEach(
+    elem => {
+      elem.style = 'overflow-y: auto !important;';
+      elem.classList.remove('js-perfect-scrollbar');
+    },
+  );
+  Array.from(document.querySelectorAll('[href]:not(link)')).forEach(elem =>
+    elem.attributes.removeNamedItem('href'),
+  );
+  Array.from(document.querySelectorAll('[href]')).forEach(elem => {
+    // eslint-disable-next-line no-self-assign
+    elem.href = elem.href;
+    // eslint-disable-next-line no-self-assign
+    elem.src = elem.src;
+  });
+};
+
+const getBoardSlug = () => {
+  return window.location.href.split('/').pop();
+};
+
+const getStylesheetList = () => {
+  return Array.from(
+    document.querySelectorAll('link[href][rel="stylesheet"]'),
+  );
+};
+
+const downloadStylesheets = async (stylesheets, zip) => {
+  await asyncForEach(stylesheets, async elem => {
+    const response = await fetch(elem.href);
+    const responseBody = await response.text();
+
+    const finalResponse = responseBody.replace(
+      new RegExp('packages\/[^\/]+\/upstream\/', 'gim'), '../'
+    );
+
+    const filename = elem.href
+      .split('/')
+      .pop()
+      .split('?')
+      .shift();
+    const fileFullPath = `style/${filename}`;
+    zip.file(fileFullPath, finalResponse);
+    elem.href = `../${fileFullPath}`;
+  });
+};
+
+const getSrcAttached = () => {
+  return Array.from(document.querySelectorAll('[src]'));
+};
+
+const downloadSrcAttached = async (elements, zip, boardSlug) => {
+  await asyncForEach(elements, async elem => {
+    const response = await fetch(elem.src);
+    const responseBody = await response.blob();
+    const filename = elem.src
+      .split('/')
+      .pop()
+      .split('?')
+      .shift();
+    const fileFullPath = `${boardSlug}/${elem.tagName.toLowerCase()}/${filename}`;
+    zip.file(fileFullPath, responseBody);
+    elem.src = `./${elem.tagName.toLowerCase()}/${filename}`;
+  });
+};
+
+const addBoardHTMLToZip = (boardSlug, zip) => {
+  ensureSidebarRemoved();
+  const htmlOutputPath = `${boardSlug}/index.html`;
+  zip.file(htmlOutputPath, new Blob([
+    removeAnchors(getPageHtmlString())
+  ], { type: 'application/html' }));
 };
 
 BlazeComponent.extendComponent({
@@ -213,51 +354,26 @@ Template.boardMenuPopup.events({
   'click .js-import-board': Popup.open('chooseBoardSource'),
   'click .js-subtask-settings': Popup.open('boardSubtaskSettings'),
   'click .js-card-settings': Popup.open('boardCardSettings'),
-  'click .html-export-board'(event) {
+  'click .html-export-board': async event => {
     event.preventDefault();
 
-    Array.from(document.querySelectorAll('script')).forEach(elem => elem.remove());
-    Array.from(document.querySelectorAll('link:not([rel="stylesheet"])')).forEach(elem => elem.remove());
-    document.querySelector('#header-quick-access').remove();
-    Array.from(document.querySelectorAll('#header-main-bar .board-header-btns')).forEach(elem => elem.remove());
-    Array.from(document.querySelectorAll('.list-composer')).forEach(elem => elem.remove());
-    Array.from(document.querySelectorAll('.list-composer,.js-card-composer, .js-add-card')).forEach(elem => elem.remove());
-    Array.from(document.querySelectorAll('.js-perfect-scrollbar > div:nth-of-type(n+2)')).forEach(elem => elem.remove());
-    Array.from(document.querySelectorAll('.js-perfect-scrollbar')).forEach(elem => {
-      elem.style = 'overflow-y: auto !important;';
-      elem.classList.remove('js-perfect-scrollbar');
-    });
-    Array.from(document.querySelectorAll('[href]:not(link)')).forEach(elem => elem.attributes.removeNamedItem('href'));
-    Array.from(document.querySelectorAll('[href]')).forEach(elem => {elem.href = elem.href; elem.src = elem.src});
-    let htmlDoc = ('<!doctype html>' + window.document.querySelector('html').outerHTML);
-    htmlDoc = htmlDoc.replace('<a ', '<span ');
-    htmlDoc = htmlDoc.replace('</a>', '</span>');
-    let dl = document.createElement('a');
-    dl.href = window.URL.createObjectURL(
-      new Blob([htmlDoc], {type: 'application/html'}) // file content
-    );
-    dl.onclick = event => document.body.removeChild(event.target);
-    dl.style.display = 'none';
-    dl.target = '_blank';
-    dl.download = window.location.href.split('/').pop() + ".html";
-    document.body.appendChild(dl);
+    const zip = new JSZip();
+    const boardSlug = getBoardSlug();
 
-    Array.from(document.querySelectorAll('link[href][rel="stylesheet"]')).forEach(async elem => {
-      let response = await fetch(elem.href)
-      let responseBody = await response.text()
-      console.log(elem.href.split('/').pop().split('?').shift())
-      console.log(responseBody)
-    });
-    Array.from(document.querySelectorAll('[src]')).forEach(async elem => {
-      let response = await fetch(elem.src)
-      let responseBody = await response.blob()
-      console.log(elem.href.split('/').pop().split('?').shift())
-      console.log(responseBody)
-    });
+    await addJsonExportToZip(zip, boardSlug);
+    Popup.close();
+    closeSidebar();
+    cleanBoardHtml();
 
-    dl.click();
+    await downloadStylesheets(getStylesheetList(), zip);
+    await downloadSrcAttached(getSrcAttached(), zip, boardSlug);
+
+    addBoardHTMLToZip(boardSlug, zip);
+
+    const content = await zip.generateAsync({ type: 'blob' });
+    saveAs(content, `${boardSlug}.zip`);
     window.location.reload();
-  },
+  }
 });
 
 Template.boardMenuPopup.onCreated(function() {
